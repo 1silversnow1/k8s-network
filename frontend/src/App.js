dbf00c70564d19622e96463071f112e01f2fe91f
vsniehurov <vsniehurov@lohika.com>
2023-05-17 11:50:11 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import './App.css';
+import TaskList from './components/TaskList';
+import NewTask from './components/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = useCallback(function () {
+    // We can use /api instead of hardcoding IP of a users-api service because we configure REVERSE PROXY for nginx
+    // Browser executes script on a client side, so you cannot reffer to k8s services using internal DNS system
+    // But, nginx is executed inside a k8s, meaning it knows about k8s DNS system
+    // That means we can make requests from frontend app to the proxy that redirects to the k8s service
+    fetch('/api/tasks', {
+      headers: {
+        'Authorization': 'Bearer abc'
+      }
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        setTasks(jsonData.tasks);
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      fetchTasks();
+    },
+    [fetchTasks]
+  );
+
+  function addTaskHandler(task) {
+    fetch('/api/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer abc',
+      },
+      body: JSON.stringify(task),
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (resData) {
+        console.log(resData);
+      });
+  }
+
+  return (
+    <div className='App'>
+      <section>
+        <NewTask onAddTask={addTaskHandler} />
+      </section>
+      <section>
+        <button onClick={fetchTasks}>Fetch Tasks</button>
+        <TaskList tasks={tasks} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
